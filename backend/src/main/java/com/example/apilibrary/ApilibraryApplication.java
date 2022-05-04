13d312883432676aa76aa2afee9ff75d2ce6cd91
lilicrst <lisandra.crisilva37@gmail.com>
2022-05-04 17:54:06 -0300

    inicialização spring e react

---
+++ backend/src/main/java/com/example/apilibrary/ApilibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.apilibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilibraryApplication.class, args);
+	}
+
+}
