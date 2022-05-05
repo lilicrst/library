0c15fdb9d092e065f64c2c3181561a75c9d8c8d8
Claudio Jose <cjdgc13@gmail.com>
2022-05-05 15:27:56 -0300

    Barra Superior

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import './styles.css';
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="urlibrary-nav-content">
+          <h1>UR Library</h1>
+          <a
+            href="https://github.com/lilicrst/library"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="urlibrary-contact-container">
+              <GithubIcon />
+              <p className="urlibrary-contact-link">/lilicrst</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
