4c6bac74f563a13cca00cc57dff49b3ab075a682
lilicrst <lisandra.crisilva37@gmail.com>
2022-05-06 07:37:15 -0300

    requisição pelo frontend

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,15 +1,23 @@
 import { ReactComponent as Arrow } from 'assets/img/arrow.svg'
+import { BookPage } from 'types/book';
 import './styles.css'
 
-function Pagination() {
+type Props = {
+  page: BookPage
+  onChange: Function;
+}
+
+function Pagination({ page, onChange } : Props) {
   return (
     <div className="urlibrary-pagination-container">
       <div className="urlibrary-pagination-box">
-        <button className="urlibrary-pagination-button" disabled={true}>
+        <button className="urlibrary-pagination-button" 
+        disabled={page.first} onClick={() => onChange(page.number - 1)}>
           <Arrow />
         </button>
-        <p>{`${1} de ${3}`}</p>
-        <button className="urlibrary-pagination-button" disabled={false}>
+        <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+        <button className="urlibrary-pagination-button" 
+        disabled={page.last} onClick={() => onChange(page.number + 1)} >
           <Arrow className="urlibrary-flip-horizontal" />
         </button>
       </div>
